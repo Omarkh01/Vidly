4e2dc8329656f52102c01bb2c26aeeac7ec26ee5
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-23 16:15:41 -0500

    committing changes

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,5 +1,5 @@
 const { Rental, validate } = require('../models/rental');
-const {Movie} = require('../models/rental');
+const {Movie} = require('../models/movie');
 const {Customer} = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -14,19 +14,31 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
     
-    const genre = await Genre.findById(req.body.genreId);
-    if(!genre) return res.status(400).send('Invalid genre..'); 
+    const customer = await Customer.findById(req.body.customerId);
+    if(!customer) return res.status(400).send('Invalid customer..');
+    
+    const movie = await Movie.findById(req.body.movieId);
+    if(!movie) return res.status(400).send('Invalid movie..');
+    
+    if(movie.numberInStock === 0) return res.status(400).send('Movie not in stock..');
 
     let rental = new Rental({
-        title: req.body.title,
-        genre: {
-            _id: genre._id,
-            name: genre.name
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone
         },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate
+        }
     });
     rental = await rental.save();
+
+    movie.numberInStock--;
+    movie.save();
+
     res.send(rental);
 })
 
