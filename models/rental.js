0fada7ba61f81374f1b5f242cd47b1f2ed3328d7
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-23 15:56:42 -0500

    committing changes

---
+++ models/rental.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Rental = mongoose.Schema('Rental', new mongoose.model({
+    customer: {
+        type: new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+                minlength: 2,
+                maxlength: 30
+            },
+            isGold: {
+                type: Boolean,
+                default: false
+            },
+            phone: {
+                type: String,
+                required: true,
+                minlength: 9,
+                maxlength: 20
+            }
+        }),
+        required: true
+    }, 
+    movie: {
+        type: new mongoose.Schema({
+            title: {
+                type: String,
+                required: true,
+                trim: true,
+                minlength: 5,
+                maxlength: 255
+            },
+            dailyRentalRate: {
+                type: Number,
+                required: true,
+                min: 0,
+                max: 255
+            }
+        }),
+        required: true
+    },
+    dateOut: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    dateReturned: {
+        type: Date
+    },
+    rentalFee: {
+        type: Number,
+        min: 0
+    }
+}))
+
+function validateRental(rental) {
+    const schema = {
+        customerId: Joi.string().required(),
+        movieId: Joi.string().required()
+    }
+    return Joi.validate(rental, schema);
+}
+
+exports.validate = validateRental;
+exports.Rental = Rental;
---
+++ routes/rentals.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const { Rental, validate } = require('../models/rental');
+const router = express.Router();
