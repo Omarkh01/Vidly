b9709a0290f9bb268c180bc998dfc5ea9e440f93
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-23 16:00:34 -0500

    committing changes

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,3 +1,7 @@
-const express = require('express');
 const { Rental, validate } = require('../models/rental');
+const {Movie} = require('../models/movie');
+const {Customer} = require('../models/customer');
+const mongoose = require('mongoose');
+const express = require('express');
 const router = express.Router();
+
