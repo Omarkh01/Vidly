ff920ac53dfcfa2b5f7c43dc54d61e6b5e395ab6
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-24 12:53:03 -0500

    added an authentication file and json web token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const app = express();
 const mongoose = require('mongoose');
 
@@ -19,6 +20,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
 
