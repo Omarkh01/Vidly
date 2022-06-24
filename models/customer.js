bd5d5b6e706e883322f35aa25dce1173d75b4edf
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-24 00:25:16 -0500

    added users api

--- models/customer.js
+++ models/customer.js
@@ -5,8 +5,8 @@ const customerSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        minlength: 2,
-        maxlength: 30
+        minlength: 5,
+        maxlength: 50
     },
     isGold: {
         type: Boolean,
