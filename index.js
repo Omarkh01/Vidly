8a4e4b426600141572fc520343282064a1bbebb8
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-25 22:07:49 -0500

    handling errors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -27,6 +28,8 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+
+app.use(error);
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
 
--- routes/genres.js
+++ routes/genres.js
@@ -4,9 +4,14 @@ const express = require('express');
 const router = express.Router();
 const {Genre, validate} = require('../models/genre')
 
-router.get('/', async (req, res) => {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
+router.get('/', async (req, res, next) => {
+    try {
+        const genres = await Genre.find().sort('name');
+        res.send(genres);
+    }
+    catch (ex) {
+        next(ex);
+    }
 })
 
 
