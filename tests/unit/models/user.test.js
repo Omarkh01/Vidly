718fc92724288543325e9c848ac920df00c7a5b5
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-29 22:24:00 -0500

    changes

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 describe('user.generateAuthToken', () => {
     it('should return a valid JWT', () => {
         const payload = {
-            _id: new mongoose.Types.ObjectId().toHexString(),
+            _id: new mongoose.Types.ObjectId().toHexString(), // essential method to hex string 
             isAdmin: true
         };
         const user = new User(payload);
