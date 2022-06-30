58da2b8456c4175af6ca16b5f236c8e151d52be8
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-06-29 22:26:50 -0500

    changes

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -6,11 +6,12 @@ const mongoose = require('mongoose');
 // unit testing for user model
 describe('user.generateAuthToken', () => {
     it('should return a valid JWT', () => {
+        const user = new User(payload); 
+        // we need to know the payload ahead of time to compare with the token we have so we store it in PAYLOAD constant
         const payload = {
             _id: new mongoose.Types.ObjectId().toHexString(), // essential method to hex string 
             isAdmin: true
         };
-        const user = new User(payload);
         const token = user.generateAuthToken();
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
         expect(decoded).toMatchObject(payload);
