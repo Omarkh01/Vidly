925f661da92a19b4848568435080d18e15ba4947
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-07-02 23:26:23 -0500

    changes

--- routes/genres.js
+++ routes/genres.js
@@ -24,7 +24,9 @@ router.put('/:id', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true});
+    const genre = await Genre.findByIdAndUpdate(req.params.id, 
+        {name: req.body.name},
+        {new: true});
     if(!genre) return res.status(404).send('The genre with the given id was not found.');
     
     res.send(genre);
