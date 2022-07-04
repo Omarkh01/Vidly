d136aae2ddc962b88280c0fe8094c532fade2fcf
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-07-03 20:53:36 -0500

    changes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const winston = require('winston');
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const config = require('config');
 const app = express();
 
+require('dotenv').config();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
@@ -11,15 +10,8 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-const uri = config.get('db');
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("production").collection("devices");
- // perform actions on the collection object
-  client.close();
-});
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,20 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
+require('dotenv').config();
 
 module.exports = function() {
     const db = config.get('db');
-    mongoose.connect(db)
-        .then(() => winston.info(`Connected to ${db}...`)); 
+    if(process.env.STATUS === 'production' ) {
+        mongoose.connect(process.env.DATABASE, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => console.log('Connected to production DATABASE...')); 
+    }
+
+    else {
+        mongoose.connect(db)
+        .then(() => console.log(`Connected to ${db}...`));
+    }
 }
